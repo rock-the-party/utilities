630f606b5c107fef0e98f54c8826ed23bdb80663
justplainfred <justplainfred@gmail.com>
2023-04-11 01:54:42 -0600

    formating

--- src/angles.ts
+++ src/angles.ts
@@ -13,22 +13,22 @@ export function radianNormalizedAngle(radianAngle:number): number {
 }
 
 export function radianHalfAngle(radianAngleA: number, radianAngleB: number): number {
-    let uX = Math.sin(radianAngleA);
-    let uY = Math.cos(radianAngleA);
-    let vX = Math.sin(radianAngleB);
-    let vY = Math.cos(radianAngleB);
-    let dotProduct = (uX * vX) + (uY * vY);
-    let divisor = Math.sqrt(Math.pow(uX, 2) + Math.pow(uY, 2)) * Math.sqrt(Math.pow(vX, 2) + Math.pow(vY, 2));
-    let angleDiff = dotProduct/divisor;
-    let angle;
+  let uX = Math.sin(radianAngleA);
+  let uY = Math.cos(radianAngleA);
+  let vX = Math.sin(radianAngleB);
+  let vY = Math.cos(radianAngleB);
+  let dotProduct = (uX * vX) + (uY * vY);
+  let divisor = Math.sqrt(Math.pow(uX, 2) + Math.pow(uY, 2)) * Math.sqrt(Math.pow(vX, 2) + Math.pow(vY, 2));
+  let angleDiff = dotProduct/divisor;
+  let angle;
 
-    if (angleDiff > 1) {
-      angle = Math.acos(1);
-    } else if (angleDiff < -1) {
-      angle = Math.acos(-1);
-    } else {
-      angle = Math.acos(angleDiff);
-    }
-
-    return (Math.PI / 2 - angle) / (Math.PI / 2);
+  if (angleDiff > 1) {
+    angle = Math.acos(1);
+  } else if (angleDiff < -1) {
+    angle = Math.acos(-1);
+  } else {
+    angle = Math.acos(angleDiff);
   }
+
+  return (Math.PI / 2 - angle) / (Math.PI / 2);
+}
