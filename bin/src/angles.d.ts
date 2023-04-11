f7cabc3e0d5bc900b55a08680f673521da098289
justplainfred <justplainfred@gmail.com>
2023-04-11 09:56:48 -0600

    moving build to bin instead of dist

---
+++ bin/src/angles.d.ts
@@ -0,0 +1,4 @@
+export declare function radiansToDegrees(radians: number): number;
+export declare function degreesToRadians(degrees: number): number;
+export declare function radianNormalizedAngle(radianAngle: number): number;
+export declare function radianHalfAngle(radianAngleA: number, radianAngleB: number): number;
---
+++ bin/src/uuid.d.ts
@@ -0,0 +1,2 @@
+export declare function generateUUID(): string;
+export declare function isUUID(str: string): boolean;
